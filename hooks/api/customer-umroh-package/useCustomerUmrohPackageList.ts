7b72a24b62d67cd6148bb8a778df8cbf0b113927
MirrorBottle <setiawanbayu66152@gmail.com>
2023-11-22 15:57:16 +0800

    feat: umroh package binding

--- hooks/api/customer-umroh-package/useCustomerUmrohPackageList.ts
+++ hooks/api/customer-umroh-package/useCustomerUmrohPackageList.ts
@@ -1,42 +1,48 @@
-import { queryKeyCustomerTransactionsHistory } from '@apps/packages/lib/constants'
+import { queryKeyCustomerUmrohPackage } from '@apps/packages/lib/constants'
 import apiServices from '@apps/packages/services'
 import { placeholderListBuilder } from '@apps/packages/services/BaseResponse'
 import {
-  CustomerTransactionsHistoryParams,
-  CustomerTransactionsHistoryResponse,
-  CustomerTransactionsHistoryResponseSchema,
-} from '@apps/packages/services/customer-transactions-history'
+  CustomerUmrohPackageResponse,
+  CustomerUmrohPackageParams,
+  CustomerUmrohPackageResponseSchema,
+} from '@apps/packages/services/customer-umroh-package'
 import { apiResponseValidation } from '@apps/packages/utils'
 import { QueryKey, UseQueryOptions, useQueryClient, useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 
-type useCustomerTransactionHistoryListConfig = {
+type useCustomerUmrohPackageConfig = {
   queryKey?: QueryKey
-  params?: CustomerTransactionsHistoryParams
-  options?: UseQueryOptions<CustomerTransactionsHistoryResponse>
+  params?: CustomerUmrohPackageParams
+  options?: UseQueryOptions<CustomerUmrohPackageResponse>
 }
 
-export const useCustomerTransactionHistoryList = (opt?: useCustomerTransactionHistoryListConfig) => {
-  const {
-    queryKey = [queryKeyCustomerTransactionsHistory.CUSTOMER_TRANSACTIONS_HISTORY_LIST],
-    params = { take: 3, pointer: 1 },
-    options,
-  } = opt ?? {}
+export const placeholderData: CustomerUmrohPackageResponse = {
+  meta: {
+    code: 200,
+    message: '',
+    response_time: 0,
+    success: true,
+  },
+  result: [],
+}
+
+export const useCustomerUmrohPackage = (opt?: useCustomerUmrohPackageConfig) => {
+  const { queryKey = [queryKeyCustomerUmrohPackage.CUSTOMER_UMROH_PACKAGE_LIST], params = {}, options } = opt ?? {}
   const queryClient = useQueryClient()
-  const placeholderData: CustomerTransactionsHistoryResponse = useMemo(
+  const placeholderData: CustomerUmrohPackageResponse = useMemo(
     () => queryClient.getQueryData(queryKey) ?? placeholderListBuilder(),
     []
   )
 
   return useQuery({
     queryKey,
-    queryFn: () => apiServices.customerTransactionsHistory.getList({ params }),
+    queryFn: () => apiServices.customerUmrohPackage.getList({ params }),
     refetchOnWindowFocus: false,
     placeholderData,
     select: (response) => {
       return apiResponseValidation({
         response,
-        schema: CustomerTransactionsHistoryResponseSchema,
+        schema: CustomerUmrohPackageResponseSchema,
         placeholderData,
       })
     },
