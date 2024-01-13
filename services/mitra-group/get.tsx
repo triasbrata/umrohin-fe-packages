f88fa30b51950defb74f8b68a845b024d159e6df
MirrorBottle <setiawanbayu66152@gmail.com>
2024-01-13 10:17:58 +0800

    feat: add debounce select component

--- services/mitra-group/get.tsx
+++ services/mitra-group/get.tsx
@@ -34,6 +34,20 @@ export const MitraGroupListResponseSchema = httpGetListResponseSchemaBuilder(Mit
 
 export type MitraGroupListResponse = z.infer<typeof MitraGroupListResponseSchema>
 
+export const MitraGroupDetailListItemSchema = z.object({
+  group_member_id: z.string(),
+  name: z.string(),
+  role: z.string(),
+  group: z.number(),
+  status: z.union([z.literal(0), z.literal(1), z.boolean()]),
+})
+
+export type MitraGroupDetailListItem = z.infer<typeof MitraGroupDetailListItemSchema>
+
+export const MitraGroupDetailListResponseSchema = httpGetListResponseSchemaBuilder(MitraGroupDetailListItemSchema)
+
+export type MitraGroupDetailListResponse = z.infer<typeof MitraGroupDetailListResponseSchema>
+
 export const getList = async <ResponseType = MitraGroupListResponse,>({
   params,
   options,
